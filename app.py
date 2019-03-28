9bacedcc0f8fa218d93ce26c532991facf07fb71
LiuP1994 <38748465+LiuP1994@users.noreply.github.com>
2019-03-28 16:47:49 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from cassandra.cluster import Cluster
 import requests
 
@@ -10,8 +10,8 @@ app = Flask(__name__)
 #category in each month in cassandra by grab the data from UK POLICE API
 @app.route('/<mydata>', methods=['GET'])
 def hello(mydata):
-    crime_url_template ='https://data.police.uk/api/crimes-street/all-crime?lat={lat}&lng={lng}&date={data}'
-    my_latitude = '51.52369'               
+    crime_url_template ='https://data.police.uk/api/crimes-street/all-crime?lat={lat}&lng={lng}&date={data}'#api enterance
+    my_latitude = '51.52369'               #set the parameters for api
     my_longitude = '-0.0395857'
     crime_url = crime_url_template.format(lat = my_latitude,lng = my_longitude,data = mydata) #request for all the crimes in one month
     resp = requests.get(crime_url)
@@ -26,12 +26,12 @@ def hello(mydata):
     else:
         print(resp.reason)
 
-    categories = {categ["url"]:categ["name"] for categ in categories_json}
+    categories = {categ["url"]:categ["name"] for categ in categories_json}#build a dictionary from the data
     crime_category_stats = dict.fromkeys(categories.keys(), 0)
     crime_category_stats.pop("all-crime")
     for crime in crimes:
         crime_category_stats[crime["category"]] += 1        #count the number of crimes in each category
-    for key in crime_category_stats:
+    for key in crime_category_stats:#insert the name and the number for each category into the database
         rows = session.execute( """insert into crime.category(month,name,count) values('{}','{}',{})""".format(mydata,key,crime_category_stats[key]))
     return('<h1>crime data for {} is created</h1>'.format(mydata))
 
@@ -40,7 +40,7 @@ def hello(mydata):
 def profile(data,name):
     rows = session.execute( """select * from crime.category where month = '{}' and name= '{}' allow filtering""".format(data,name))
     for crime in rows:
-        return('<h1>{} in {} happened {} times!</h1>'.format(name, data, crime.count))
+        return jsonify(crime)
     return('<h1>That kind of crime does not exist!</h1>')
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
